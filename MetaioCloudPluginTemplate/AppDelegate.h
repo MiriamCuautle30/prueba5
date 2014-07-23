84aae04f9c9f22ffcba8c9b18574668e8b5afb48
Miriam Cuautle <cansys60@yahoo.com.mx>
2014-07-23 17:07:48 -0500

    xco
    casdc

---
+++ MetaioCloudPluginTemplate/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//
+//  Created by Stefan Misslinger.
+// Copyright 2007-2013 metaio GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UIViewController *viewController;
+
+@end
